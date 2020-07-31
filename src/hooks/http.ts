3ad515ccbba6cacb4d3d492bc09d0800f212ef17
Benjamin <bmarquiscom@gmail.com>
2020-07-31 10:22:38 -0400

    Update authentication logic

--- src/hooks/http.ts
+++ src/hooks/http.ts
@@ -73,7 +73,7 @@ const useHttp: UseHttp = (initialRequest) => {
     headers: {
 
       // If the AuthContext contains an authToken it will be included in the headers
-      Authorization: `Bearer ${token || 'no token'}`,
+      Authorization: `Bearer ${token || process.env.GATSBY_ACCESS_TOKEN}`,
       ...headers
     }
   });
